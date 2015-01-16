90e72ed7bfb2357400f828ff8c3adb791e5ca7ed
Christina Gunarto <cgunarto@gmail.com>
2015-01-16 11:26:07 -0800

    Initial Commit

---
+++ mealPlanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mealPlanner
+//
+//  Created by CHRISTINA GUNARTO on 1/16/15.
+//  Copyright (c) 2015 Christina Gunarto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mealPlanner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mealPlanner
+//
+//  Created by CHRISTINA GUNARTO on 1/16/15.
+//  Copyright (c) 2015 Christina Gunarto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
